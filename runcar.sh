eea6e735532d60cfea3421a780163dd93f432632
Dmitry Ananyev <d.ananyev@gmail.com>
2021-07-31 02:43:33 +0100

    better model + config

--- runcar.sh
+++ runcar.sh
@@ -2,12 +2,12 @@
 # run pln model using the docker gpu image (adapted from Toni's script)
 
 #cmd_line="python3 /root/myrace/manage.py drive --model=/root/myrace/models/mymodel.h5"
-cmd_line="python3 /root/myrace/manage.py drive --model /root/myrace/models/mypilot_circuit_launch_16.h5 --myconfig=myconfig-trnm.py"
+cmd_line="python3 /root/myrace/manage.py drive --model /root/myrace/models/mypilot_circuit_launch_19.h5 --myconfig=myconfig-trnm.py"
 
 echo "Executing in the docker (gpu image):"
 echo $cmd_line
 
 docker run -it --rm --network host \
-  altexdim/donkeycar_race2:v1 \
+  altexdim/donkeycar_race2:v2 \
   bash -c "cd /root/myrace/ && $cmd_line"
 
